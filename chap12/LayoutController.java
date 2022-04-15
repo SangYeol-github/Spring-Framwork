c250553bd6fe4e88359d2f2aefd9af477eb78969
SangYeol-github <polkmn222@naver.com>
2022-04-15 11:28:29 +0900

    정규수업

--- chap12/LayoutController.java
+++ chap12/LayoutController.java
@@ -1,8 +1,15 @@
 package com.tjoeun.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 //@RestController
@@ -12,17 +19,48 @@ public class LayoutController {
 	@GetMapping("/lay")
 	public String layout() {
 		
-//		return "Layout Test";
 		return "layout/lay";
 	}
 	
 	@GetMapping("/lay2")
 	public String layout2() {
 		
-//		return "Layout Test";
 		return "layout/lay2";
 	}
 	
+	@GetMapping("/lay3")
+	public String layout3() {
+
+		return "layout/lay3";
+	}
+	
+	@GetMapping("/lay4")
+	public String layout4() {
+		
+		return "layout/lay4";
+	}
+	
+	@PostMapping("/lay4")
+	@ResponseBody
+	public Map<String, String> layout44(@RequestParam String hobby) {
+		
+		String msg = hobby + "를 선택했군요...";
+		Map<String, String> map = new HashMap<>();
+		map.put("msg", msg);
+		return map;
+	}
+	
+	@PostMapping("/arr")
+	@ResponseBody
+	public Map<String,String> procArray(
+			@RequestParam(value="delfile[]") List<String> delfile) {
+		
+		Map<String,String> map = new HashMap<>();
+		for(int i=0;i<delfile.size();i++) {
+			map.put(""+i, delfile.get(i));
+		}
+		return map;
+	}
 	
 	
 	
