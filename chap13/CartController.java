9da7afd051326a43926d3ca26df661832b5f5467
SangYeol-github <polkmn222@naver.com>
2022-04-22 11:54:16 +0900

    정규수업

--- chap13/CartController.java
+++ chap13/CartController.java
@@ -1,10 +1,9 @@
 package com.tjoeun.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,43 +12,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.tjoeun.svc.CartSVC;
 import com.tjoeun.vo.BookItem;
 
 @Controller
 @RequestMapping("/shop")
+@SessionAttributes("cart")
 public class CartController {
-	
 	@Autowired
 	private CartSVC svc;
-	
+
 	@GetMapping("/book")
-	public String showBook(HttpSession session) {
-		svc.setSession(session);
+	public String showBook(Model model, 
+			@SessionAttribute(value="cart", required=false) List<BookItem> cart) {
+		// cart를 생성하여 model에 저장하면 @SessionAttributes("cart")에 "cart" 란 이름을
+		// 지정했기 때문에 HttpSession에도 자동으로 저장된다
+		if(cart==null) // 아직 세션에 cart 가 생성되어 있지 않은 경우에만 cart를 생성한다
+		{
+			model.addAttribute("cart", new ArrayList<BookItem>());
+		}
 		return "/cart/book";
 	}
 	
 	@PostMapping("/add")
 	@ResponseBody
-	public Map<String, Boolean> cartAdd(BookItem book, HttpServletRequest request) {
-		boolean added = svc.addItem(book, request);
+	public Map<String, Boolean> cartAdd(BookItem book, 
+			@SessionAttribute("cart") List<BookItem> cart) {
+		boolean added = svc.addItem(book, cart);
 		Map<String,Boolean> map = new HashMap<>();
 		map.put("added", added);
 		return map;
 	}
 	
 	@GetMapping("/list")
-	public String showCart(Model model, HttpServletRequest request) {
-		model.addAttribute("list", svc.getCart(request));
-		model.addAttribute("total", svc.getTotalPrice(request));
+	public String showCart(Model model, @SessionAttribute("cart") List<BookItem> cart) {
+		model.addAttribute("list", cart);
+		model.addAttribute("total", svc.getTotalPrice(cart));
 		return "/cart/showList";
 	}
 	
 	@PostMapping("/order")
 	@ResponseBody
-	public Map<String, Boolean> orderBook(HttpServletRequest request) {
-		boolean ordered = svc.order(request);
+	public Map<String, Boolean> orderBook(@SessionAttribute("cart") List<BookItem> cart) {
+		boolean ordered = svc.order(cart);
 		Map<String,Boolean> map = new HashMap<>();
 		map.put("ordered", ordered);
 		return map;
--- chap13/CartSVC.java
+++ chap13/CartSVC.java
@@ -2,10 +2,6 @@ package com.tjoeun.svc;
 
 import java.util.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,20 +9,9 @@ import com.tjoeun.vo.BookItem;
 
 @Service
 public class CartSVC {
-	private HttpSession session;
-
-	public void setSession(HttpSession session) {
-		if(this.session==null) {
-			this.session = session;
-			List<BookItem> cart = new ArrayList<>();
-			this.session.setAttribute("cart", cart);
-			System.out.println("cart 생성, 세션에 저장");
-		}
-	}
-	
-	public boolean addItem(BookItem book, HttpServletRequest request) {
+	public boolean addItem(BookItem book, List<BookItem> cart)
+	{
 		//동일 아이템이 이미 장바구니에 들어 있는지 확인
-		List<BookItem> cart = (List<BookItem>) request.getSession().getAttribute("cart");
 		if(cart.contains(book)) {
 			int idx = cart.indexOf(book);
 			cart.get(idx).setQty( cart.get(idx).getQty() + book.getQty() );
@@ -35,14 +20,9 @@ public class CartSVC {
 		cart.add(book);
 		return true;
 	}
-	
-	public List<BookItem> getCart(HttpServletRequest request) {
-		List<BookItem> cart = (List<BookItem>) request.getSession().getAttribute("cart");
-		return cart;
-	}
-	
-	public int getTotalPrice(HttpServletRequest request) {
-		List<BookItem> cart = (List<BookItem>) request.getSession().getAttribute("cart");
+
+	public int getTotalPrice(List<BookItem> cart)
+	{
 		int total = 0;
 		for(int i=0;i<cart.size();i++) {
 			total += cart.get(i).getPrice() * cart.get(i).getQty();
@@ -51,10 +31,8 @@ public class CartSVC {
 	}
 	
 	@Transactional(rollbackFor={Exception.class})
-	public boolean order(HttpServletRequest request) {
-		List<BookItem> cart = (List<BookItem>) request.getSession().getAttribute("cart");
-		for(int i=0;i<cart.size();i++)
-		{
+	public boolean order(List<BookItem> cart) {
+		for(int i=0;i<cart.size();i++) {
 			//dao.save(cart.get(i));
 		}
 		return true;
